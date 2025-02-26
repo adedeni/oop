aca854afcc202bfde864e715246206ad1d6301c3
adedeni <161731890+adedeni@users.noreply.github.com>
2025-02-26 20:09:58 +0100

    i have set up the database the file structure and currently working on init.php

---
+++ core/init.php
@@ -0,0 +1,13 @@
+ <?php
+ session_start();
+
+ $GLOBALS['config'] = [
+    'mysql' => [
+        'host' => '127.0.0.1',
+        'username' =>'root',
+        'password' => '',
+        'db' => 'learning_oop'
+    ],
+    'remember' => [],
+    'session' => []
+ ];
