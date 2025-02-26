4a8a9ec4ed34c186f3c7878d62cb5571a5f2bf4d
adedeni <161731890+adedeni@users.noreply.github.com>
2025-02-26 22:10:07 +0100

    finished init.php now want to require it at the beggining of each file

--- core/init.php
+++ core/init.php
@@ -3,11 +3,23 @@
 
  $GLOBALS['config'] = [
     'mysql' => [
-        'host' => '127.0.0.1',
+        'host' => '127.0.0.1', //localhost address
         'username' =>'root',
         'password' => '',
-        'db' => 'learning_oop'
+        'db' => 'learning_oop' //database name
     ],
-    'remember' => [],
-    'session' => []
+    'remember' => [
+        'cookie_name' => 'hash', //you can name it anything
+        'cookie_expiry' => 604800 //cookie lapse for a week 
+    ],
+    'session' => [
+        'session_name' => 'user', 
+    ]
  ];
+
+ //THIS IS AUTOLOAD FUNCTION
+spl_autoload_register(function($class){
+    require_once 'classes/' . $class . '.php';
+});
+
+require_once 'functions/sanitize.php';
