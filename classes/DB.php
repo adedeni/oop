6770cbd23d68ac89776bdf3a7670d1cb3732966f
adedeni <161731890+adedeni@users.noreply.github.com>
2025-03-04 14:40:20 +0100

    i have set up the database and querying the database successfully

--- classes/DB.php
+++ classes/DB.php
@@ -8,11 +8,18 @@ class DB {
             $_count = 0;  
 
 private function __construct() {//contructor is used to run the operation of the class at every instantiating
-    try{
-        $this->_pdo = new PDO('mysql:host=' . config::get('mysql/host') . ';dbname=' . config::get('mysql/db'), config::get('mysql/username'), config::get('mysql/password'));
-        //echo '<br>  Connected';
-    } catch(PDOException $e){
-        die($e->getMessage());
+    try {
+        $this->_pdo = new PDO('mysql:host=' . Config::get('mysql/host') . ';dbname=' . Config::get('mysql/db'), config::get('mysql/username'), Config::get('mysql/password'));
+        //echo 'this DB is 
+        // connected';//this is 
+        // to check if the 
+        // connection is 
+        // successful
+        $this->_pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch(PDOException $e) {
+        $this->_error = true;
+        error_log($e->getMessage());
+        return;
     }
 }
 //THIS IS TO CHECK IF WE HAVE INSTANTIATE THE OBJECT AND INSTANTIATE IF NOT
@@ -23,24 +30,56 @@ public static function getInstance() {
     return self::$_instance;
 }
 public function query($sql, $params = []){
-    $this->_error = false; //reset the error back to false
-    if($this->_query = $this->_pdo->prepare($sql)){//prepare query
-        echo "success";
-        exit ();
-        $x = 1;
-        if(count($params)){
-            foreach($params as $param){
-                $this->_query->bindValue($x, $param);
-                $x++;
+    $this->_error = false; //reset the error back to false, because we can perform multiple queries and we do not want to return the error of the previous query
+    try {
+        if($this->_query = $this->_pdo->prepare($sql)){//check if query was prepared successfully
+            //echo "query prepared success";//this is to check if the query was prepared successfully
+            //exit ();
+            $x = 1;//this is the position of the parameter and $param is the value of the parameter
+            if(count($params)){
+                foreach($params as $param){
+                    $this->_query->bindValue($x, $param);
+                    $x++;//this is to increment the position of the parameter for each loop
+                }
+            }
+            //this is to execute the query even when there is no parameter, just execute the query anyway
+            if($this->_query->execute()){
+                //echo "query executed successfully";//this is to check if the query was executed successfully
+                $this->_results = $this->_query->fetchAll(PDO::FETCH_OBJ);//store the results in the $_results property
+                $this->_count = $this->_query->rowCount();
+            } else {
+                $this->_error = true;//set the error to true if the query is not executed successfully
             }
         }
-        if($this->_query->execute()){
-            $this->_results = $this->_query->fetchAll(PDO::FETCH_OBJ);
-            $this->_count = $this->_query->rowCount();
-        } else {
-            $this->_error = true;
+    } catch(PDOException $e) {
+        $this->_error = true;
+        error_log($e->getMessage());
+    }
+    return $this;//this is to return the object of the class
+}
+public function action($action, $table, $where = []){//this is to perform any action on the datebase, e.g get, delete, update, insert
+    if(count($where) === 3){
+        $operators = ['=', '>', '<', '>=', '<='];//this are the operators that can be used in the query
+        $field = $where[0];
+        $operator = $where[1];
+        $value = $where[2];
+        if(in_array($operator, $operators)){//to check if the operator is in the array of operators
+            $sql = "{$action} FROM {$table} WHERE {$field} {$operator} ?";//this is just like writing "(SELECT * =$action)FROM (users =$table)WHERE (=$field) (= =$operator) '(adedeni= value in this case ?)'"
+            if(!$this->query($sql, [$value])->error()){
+                return $this;
+            }
         }
     }
-    return $this;
 }
+public function get($table, $where){//this is to get data from the database
+}
+public function delete($table, $where){//this is to delete data from the database
+}
+public function error(){
+    return $this->_error;//this by default returns false and if there has beenn error this method will return true, remember that the error property is set to false by default at the top of the class
+}
+public function count(){
+   return $this->_count;
+}
+
 }
--- index.php
+++ index.php
@@ -5,13 +5,21 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-<body>
+<body>    
     <?php
-    require_once 'core/init.php';
-    echo "<br>"; 
-    var_dump(config::get('mysql/host')); ;
-    echo "<br>";
-    DB::getInstance();// THIS CONNECT TO DATABASE
+    require_once 'core/init.php'; 
+    //var_dump(config::get('mysql/host'));//this is to check if the config is loaded successfully
+   
+    $user = DB::getInstance()->get('users', ['username', '=', 'adedeni']);// THIS CONNECT TO DATABASE, and make it easier to write the query since we have binded the parameters in the DB class and passed values already, a get method is used to get data from the database, the delete method is used to delete data from the database, the update method is used to update data in the database, the insert method is used to insert data into the database
+    //var_dump($user->error());
+    if($user->error()){
+        
+        echo "There was an error";
+    } else {
+        echo "There was no error";
+    }
+    
+
     ?>
 </body>
 </html>
